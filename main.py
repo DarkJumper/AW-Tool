c91640e6061628107e56c00c6a2284c66637fd0c
Peter Schwarz <p.schwarz1994@outlook.de>
2020-12-27 13:53:23 +0100

    erstellen Dataclass

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import yaml
+from dataclasses import dataclass
 
-if __name__ == "__main__":
+
+@dataclass
+class AW:
+    msr: dict
+    et: dict
+    pls: dict
+    faktor: dict
+
+
+def getDatafromYaml():
     with open('Verrechnung.yaml', 'r') as f:
         data = yaml.load(f)
-        print("HALLO?!")
-"""         print("__________vorOrt__________")
-        print(data["Verrechnung"]["MSR"]["vor Ort"])
-        print("__________leitstand__________")
-        print("__________erster Kenn________")
-        print(data["Verrechnung"]["MSR"]["Leitstand"][1])
-        print("__________zweiter Kenn________")
-        print(data["Verrechnung"]["MSR"]["Leitstand"][2])
-        print("__________Weitere________")
-        print(data["Verrechnung"]["MSR"]["weitere"])
-        print("__________ET________")
-        print(data["Verrechnung"]["ET"])
-        print("__________PLS________")
-        print(data["Verrechnung"]["PLS"])
-        print("__________mehrung________")
-        print(data["Verrechnung"]["Mehrung"]) """
+        AW(
+            data["Verrechnung"]["MSR"], data["Verrechnung"]["ET"], data["Verrechnung"]["PLS"],
+            data["Verrechnung"]["Mehrung"]
+            )
+
+
+if __name__ == "__main__":
+    getDatafromYaml()
